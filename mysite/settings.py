1daac78f7e326edd8b546976d97eb8c8cac9cf8d
Iana <jredkach@gmail.com>
2017-02-04 15:00:05 +0200

    My Django Girls app, first commi1t

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0phngjln=8k*#a49ppx4rk=cxt9qv73@bo#_z&wd_sxrbd5lgm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yana13.pythonanywhere.com']
 
 
 # Application definition
